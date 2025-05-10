68db24dcac1ff0f52369cfbc29812f527be4057c
Navyashri-Kolachippu <navya.nsc@gmail.com>
2025-05-10 22:50:15 +0530

    chat message added

---
+++ src/components/ChatMessage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const chatMessage = () => {
+  return (
+    <div className="flex items-center shadow-sm p-2">
+        <img alt="user" src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"/>
+        <span></span>
+    </div>
+  )
+}
+
+export default chatMessage
---
+++ src/components/LiveChat.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ChatMessage from './ChatMessage'
+
+const LiveChat = () => {
+  return (
+    <div className="ml-2 p-2 w-full h-[500px] border border-black bg-slate-100 rounded-lg">LiveChat
+    <ChatMessage/>
+    </div>
+    
+  )
+}
+
+export default LiveChat
